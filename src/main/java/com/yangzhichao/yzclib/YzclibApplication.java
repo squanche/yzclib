adf57ac0efbafd780cd3373d378f3e71093dd009
yangzhichao <yangzhichao@cecdat.com>
2020-01-17 14:08:46 +0800

    feat(新建项目):

---
+++ src/main/java/com/yangzhichao/yzclib/YzclibApplication.java
@@ -0,0 +1,13 @@
+package com.yangzhichao.yzclib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzclibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YzclibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yangzhichao/yzclib/YzclibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangzhichao.yzclib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YzclibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
