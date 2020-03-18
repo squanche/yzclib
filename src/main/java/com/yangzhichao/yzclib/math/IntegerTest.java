2af0f5886186b36eed2f9d61e6e6460cca3946c5
yangzhichao <837950579@qq.com>
2020-03-18 14:45:24 +0800

    添加math测试

---
+++ src/main/java/com/yangzhichao/yzclib/math/IntegerTest.java
@@ -0,0 +1,21 @@
+package com.yangzhichao.yzclib.math;
+
+import com.fasterxml.jackson.databind.util.ISO8601Utils;
+
+public class IntegerTest {
+
+
+    public static void main(String[] args) {
+
+        Integer a = 100;
+        Integer b = 100;
+        Integer c = 200;
+        Integer d = 200;
+        System.out.println(a == b);
+        System.out.println(c == d);
+
+
+    }
+
+
+}
